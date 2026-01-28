a24e2e6b9766baf7cf436dbee095bbb472bb5a70
Yugant Nagralawala <120747586+iamyugant@users.noreply.github.com>
2026-01-28 22:43:56 +0000

    exercise1

--- apputil.py
+++ apputil.py
@@ -1,13 +1,16 @@
 import numpy as np
 
+def ways(n, coin_types=[1, 5]):
+    ways_list = [0] * (n + 1)
+    ways_list[0] = 1
 
-# update/add code below ...
+    for coin in coin_types:
+        for amount in range(coin, n + 1):
+            ways_list[amount] += ways_list[amount - coin]
 
-def ways(n):
-    return None
+    return ways_list[n]
 
-def lowest_score(names, scores):
-    return None
-
-def sort_names(names, scores):
-    return None
+print(ways(12))
+print(ways(20))
+print(ways(3))
+print(ways(0))
